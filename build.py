6ce0843905d96b4c083b3a902d5ff03bb240dc58
chillfactor032 <chill@chillaspect.com>
2022-09-06 06:21:10 -0500

    Added flags to built and irc

--- build.py
+++ build.py
@@ -196,7 +196,7 @@ if(target_env == OperatingSystem.WINDOWS):
             f" -o dist/{OUTPUT_FILE} " \
             f"{input_file_path}"
 else:
-    cmd = f"pyinstaller --onefile --windowed --name='{OUTPUT_FILE}' --icon='{script_dir}/resources/img/icon_128.icns' {input_file_path}"
+    cmd = f"pyinstaller -y --onefile --windowed --name='{OUTPUT_FILE}' --icon='{script_dir}/resources/img/icon_128.icns' {input_file_path}"
     
 proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 for c in iter(lambda: proc.stdout.read(1), b''):
