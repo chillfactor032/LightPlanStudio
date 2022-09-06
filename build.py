52c90153d55b41e5570d82f2894c3b483b6b1d3e
chillfactor032 <chill@chillaspect.com>
2022-09-06 03:42:18 -0500

    Fixed typo in build.py

--- build.py
+++ build.py
@@ -195,7 +195,7 @@ if(target_env == OperatingSystem.WINDOWS):
             f" -o dist/{OUTPUT_FILE} " \
             f"{INPUT_FILE}"
 else:
-    cmd = f"pyinstaller --onefile --windowed --name='{OUTPUT_FILE}' --icon='resources/img/icons.icns' {INPUT_FILE}.py"
+    cmd = f"pyinstaller --onefile --windowed --name='{OUTPUT_FILE}' --icon='resources/img/icons.icns' {INPUT_FILE}"
     
 proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 for c in iter(lambda: proc.stdout.read(1), b''):
